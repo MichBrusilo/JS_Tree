1eeb773c85c00435bcb85b19455cbe42a34e8daf
Michał Brusiło <mibru@interia.pl>
2018-04-07 17:42:59 +0200

    Update index.html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+function drawTree(a){
+  
+    for(var i=1;i<=a;i++) {
+      
+     var star = " ";
+     for(var k=10;k>=i;k--) {
+       star += " ";
+     }
+     
+     for(var j=1;j<=i;j++) {
+     
+     star += "*";
+      
+     }
+     
+     for (var l=3;l<=j;l++) {
+       star += "*";
+     }
+      console.log(star);
+     }
+  };
+  
+drawTree(10);
